d6add2be539e6a20821b802ddd0684e213fdb70b
Sanjeev Lingam-Nattamai <slingam00@gmail.com>
2019-06-28 17:07:39 -0400

    added remove methods

--- LinkedList.py
+++ LinkedList.py
@@ -11,12 +11,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_beginning(self, newdata): #insertion at the beginning of the linked list
-        newNode = Node(newdata)
+    def insert_at_beginning(self, data): #insertion at the beginning of the linked list
+        newNode = Node(data)
         self.head = newNode
 
-    def insert_at_end(self, newdata): #insertion at the end of the linked list
-        newNode = Node(newdata)
+    def insert_at_end(self, data): #insertion at the end of the linked list
+        newNode = Node(data)
         if self.head is None:
             self.head = newNode
         else:
@@ -25,10 +25,23 @@ class LinkedList:
                 last = last.next
             last.next = newNode
 
-    def insert_in_between(self, middle, newdata): #insertion in the middle of the linked list
+    def insert_in_between(self, middle, data): #insertion in the middle of the linked list
         if middle is None:
             return
-        newNode = Node(newdata)
+        newNode = Node(data)
         newNode.next = middle.next
         middle.next = newNode
+
+    def remove_first_node(self, data):
+        if self.data is self.head:
+            self.head = self.head.next
+
+    def remove_last_node(self, data):
+        if self.data.next is None:
+            self.data = None
+
+    def remove_in_between(self, data):
+        if Node.next == self.data:
+            Node.next = self.data.next
+            self.data = None
 
