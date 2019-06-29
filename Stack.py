d984ed0c3145631b19f1d1f48f7471a67868d22e
Sanjeev Lingam-Nattamai <slingam00@gmail.com>
2019-06-29 00:53:52 -0400

    added Stack.py file

---
+++ Stack.py
@@ -0,0 +1,22 @@
+class Stack: #start of the Stack class
+
+    def __init__(self): #initializing the stack
+        self.stack = [] #starting with an empty list
+
+    def pop(self): #popping an item out of the stack
+        return self.stack.pop()
+
+    def push(self, item): #pushing an item into the stack
+        self.stack.append(item)
+
+    def size(self): #receiving the size of the stack
+        return len(self.stack)
+
+    def show(self): #displays the full stack
+        return self.stack
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return "The stack is empty" #if the stack is empty, then can't peek and so gives this message
+
+        return self.stack[-1] #otherwise, we are returning the top of the stack
